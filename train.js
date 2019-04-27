1faca6ece077e70cdc54f574b0df40187ca19992
Dan Longo <dan_longo@Dans-MBP.attlocal.net>
2019-04-27 16:17:41 -0500

    saturday commit

--- train.js
+++ train.js
@@ -8,52 +8,65 @@ var config = {
    messagingSenderId: "490979657912"
  };
  firebase.initializeApp(config);
+ console.log(firebase)
 //set variable for databse 
 var database = firebase.database();
+// train variables
+var trainName = "";
+var destination = "";
+var time = "";
+var frequency = "";
+
 //on click of submit to grab user input fields and submit to firebase
-$(document).on("click", "button", function () {
+$('#new-train').on("click", "button", function () {
     //event prevent default will keep the page from reloading and losing the information already stored from the db
     event.preventDefault();
+    console.log('clicked')
  //jquery varibles to capture the specified form fields
-    var trainName = $("#train-name").val().trim()
-    var destInput = $("#destination").val().trim()
-    var trainTime = $("#train-time").val().trim()
-    var trainFreq = $("#train-freq").val().trim()
+    trainName = $("#train-name").val().trim()
+    destination = $("#destination").val().trim()
+    trainTime = $("#train-time").val().trim()
+    trainFreq = $("#train-freq").val().trim()
  //object created to store the captured information that will be pushed to the database   
     var newTrain = {
-        //use a different name for the jquery caputure which be the value, from the key that will be stored in the db
-        name: trainName,
-        destination: destInput,
-        time: trainTime,
-        frequency: trainFreq,
+   //use a different name for the jquery caputure which be the value, from the key that will be stored in the db
+        trainName: trainName,
+        destination: destination,
+        traintime: trainTime,
+        trainfrequency: trainFreq,
 }
 //will push this newly captured information into the database, this needs to be inside the click function
 //at this point we are completed with pushing our information to the database
 //see the next database ref call that will be used to populate the DOM via jQuery with live data from the server
 database.ref().push(newTrain);
 });
+// console.log(database)
 //firebase db config to capture the existing items in the database
 database.ref().on("child_added", function (childsnapshot) {
 //create variables to grab the databse values for load into the dom
-    var dbTrain = childsnapshot.val().name;
+    var dbTrain = childsnapshot.val().trainName;
     var dbDest = childsnapshot.val().destination;
-    var dbTime = childsnapshot.val().time;
+    var dbTime = childsnapshot.val().trainTime;
     var dbFreq= childsnapshot.val().frequency;
     //calculations will be done in the app and not stored on the db to save space
 
     // Now:
-    var currenttime = moment();
-   console.log("Now TIME: " + currenttime)
+    var currentTime = moment();
+      console.log("Now TIME: " + currentTime)
+    var tfrequency = parseInt(tfrequency);
 
-    //Take a look at activity 21 train predictions create momentFreq to hold the date we are using to calculate and then specify it's format
-  
-    //  Minutes to arrival:
-   var minuteArrival = currenttime.diff(firsttimeMoment, 'minutes')
-   // Frequency (min)
-   var minuteLast = minuteArrival % frequency;
-   var trainAway = frequency - minuteLast;
-   //  Next arrival
-   var nextArrival = currenttime.add(trainAway, 'minutes');
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+    // Minutes Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
    var arrivaltime = nextArrival.format("HH:mm");
    
    //jquery variables for ease of use to append and create a new row with the captured data from the database
@@ -69,7 +82,5 @@ database.ref().on("child_added", function (childsnapshot) {
     //append the newly created row to the body of table
     tBody.append(tRow);
     
-   }, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-    });
-// $("#AddTrain").append("<tr><td>" + trainname + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + arrivaltime + "</td><td>" + trainAway + "</td>");
+// $("#new-train").append("<tr><td>" + trainname + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + arrivaltime + "</td><td>" + trainAway + "</td>");
+})

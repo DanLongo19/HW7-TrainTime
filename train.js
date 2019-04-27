d62205c2d25ec3419ab8b7d609e787440af95cac
Dan Longo <dan_longo@Dans-MBP.attlocal.net>
2019-04-27 09:20:55 -0500

    saturday 1

--- train.js
+++ train.js
@@ -1,14 +1,13 @@
 //base firebase config info for usage
 var config = {
-    apiKey: "AIzaSyBR61jxhgBVZQJLC0x7D9CkTUJnT4DbBGE",
-    authDomain: "fir-practice-227fb.firebaseapp.com",
-    databaseURL: "https://fir-practice-227fb.firebaseio.com",
-    projectId: "fir-practice-227fb",
-    storageBucket: "fir-practice-227fb.appspot.com",
-    messagingSenderId: "344231653528"
-};
-//initialize firebase
-firebase.initializeApp(config);
+   apiKey: "AIzaSyBiQwekcoI4zERqhj46b6LskL_c7PnqbAc",
+   authDomain: "train-time-6dad6.firebaseapp.com",
+   databaseURL: "https://train-time-6dad6.firebaseio.com",
+   projectId: "train-time-6dad6",
+   storageBucket: "train-time-6dad6.appspot.com",
+   messagingSenderId: "490979657912"
+ };
+ firebase.initializeApp(config);
 //set variable for databse 
 var database = firebase.database();
 //on click of submit to grab user input fields and submit to firebase
@@ -16,51 +15,61 @@ $(document).on("click", "button", function () {
     //event prevent default will keep the page from reloading and losing the information already stored from the db
     event.preventDefault();
  //jquery varibles to capture the specified form fields
-    var empName = $("#emp-name").val().trim()
-    var empRole = $("#emp-role").val().trim()
-    var empStart = $("#emp-start").val().trim()
-    var empRate = $("#emp-rate").val().trim()
+    var trainName = $("#train-name").val().trim()
+    var destInput = $("#destination").val().trim()
+    var trainTime = $("#train-time").val().trim()
+    var trainFreq = $("#train-freq").val().trim()
  //object created to store the captured information that will be pushed to the database   
-    var newEmp = {
+    var newTrain = {
         //use a different name for the jquery caputure which be the value, from the key that will be stored in the db
-        name: empName,
-        role: empRole,
-        start: empStart,
-        rate: empRate,
+        name: trainName,
+        destination: destInput,
+        time: trainTime,
+        frequency: trainFreq,
 }
-//will push this newly captured information into the databse, this needs to be inside the click function
-//at this point we are complted with pushing our information to the database
-//see the next databse ref call that will be used to populate the DOM via jQuery with live data from the server
-database.ref().push(newEmp);
+//will push this newly captured information into the database, this needs to be inside the click function
+//at this point we are completed with pushing our information to the database
+//see the next database ref call that will be used to populate the DOM via jQuery with live data from the server
+database.ref().push(newTrain);
 });
 //firebase db config to capture the existing items in the database
 database.ref().on("child_added", function (childsnapshot) {
 //create variables to grab the databse values for load into the dom
-    var dbName = childsnapshot.val().name;
-    var dbRole = childsnapshot.val().role;
-    var dbStart = childsnapshot.val().start;
-    var dbRate = childsnapshot.val().rate;
+    var dbTrain = childsnapshot.val().name;
+    var dbDest = childsnapshot.val().destination;
+    var dbTime = childsnapshot.val().time;
+    var dbFreq= childsnapshot.val().frequency;
     //calculations will be done in the app and not stored on the db to save space
-    //create momentInst to hold the date we are using to calculate and then specify it's format
-    var momentIsnt = moment(dbStart, "MM/DD/YYYY")
-    //calculate the month worked using diff- review moment.js
-    var monthsWorked = momentIsnt.diff(moment(), 'months') * -1;
-    //calculate the total billed
-    var totalBilled = monthsWorked * dbRate;
+
+    // Now:
+    var currenttime = moment();
+   console.log("Now TIME: " + currenttime)
+
+    //Take a look at activity 21 train predictions create momentFreq to hold the date we are using to calculate and then specify it's format
+  
+    //  Minutes to arrival:
+   var minuteArrival = currenttime.diff(firsttimeMoment, 'minutes')
+   // Frequency (min)
+   var minuteLast = minuteArrival % frequency;
+   var trainAway = frequency - minuteLast;
+   //  Next arrival
+   var nextArrival = currenttime.add(trainAway, 'minutes');
+   var arrivaltime = nextArrival.format("HH:mm");
    
    //jquery variables for ease of use to append and create a new row with the captured data from the database
     var tBody = $("tbody");
     var tRow = $("<tr>")
-    var nameTd = $("<td>").text(dbName)
-    var roleTd = $("<td>").text(dbRole)
-    var startTd = $("<td>").text(dbStart)
-    var mnthsTD = $("<td>").text(monthsWorked)
-    var rateTd = $("<td>").text(dbRate)
-    var billedTd = $("<td>").text(totalBilled)
-    //create a new row append the data valaues from the db
-    tRow.append(nameTd, roleTd, startTd, mnthsTD, rateTd, billedTd);
+    var trainTd = $("<td>").text(dbTrain)
+    var destTd = $("<td>").text(dbDest)
+    var timeTd = $("<td>").text(dbTime)
+    var freqTd = $("<td>").text(dbFreq)
+   
+   //create a new row append the data valaues from the db
+    tRow.append(trainTd, destTd, timeTd, freqTd, arrivaltime);
     //append the newly created row to the body of table
     tBody.append(tRow);
-      
-      
-    })
+    
+   }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
+// $("#AddTrain").append("<tr><td>" + trainname + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + arrivaltime + "</td><td>" + trainAway + "</td>");
